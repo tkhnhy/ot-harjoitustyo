b0147178cb81b378dd2fc1080117947103f5604f
TommiWindows <tommi.kohonen21@gmail.com>
2023-12-05 13:22:13 +0200

    erotettu käyttöliittymä

---
+++ src/interface/display.py
@@ -0,0 +1,9 @@
+import pygame
+
+class Display:
+    def __init__(self):
+        self.display_height = 856
+        self.display_width = 512
+
+    def give_display(self):
+        return pygame.display.set_mode((self.display_width, self.display_height))
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import pygame
-from renderer import Renderer
+from interface.renderer import Renderer
+from interface.display import Display
 from game_loop import GameLoop
 from sprites.player import Player
 from clock import Clock
@@ -10,14 +11,12 @@ def main():
 
     pygame.init()
 
-    display_height = 856
-    display_width = 512
-    display = pygame.display.set_mode((display_width, display_height))
+    display = Display()
 
     player = Player(236, 800)
     scoresystem = Highscores()
 
-    renderer = Renderer(display)
+    renderer = Renderer(display.give_display())
     clock = Clock()
 
     game = GameLoop(clock, renderer, player)
